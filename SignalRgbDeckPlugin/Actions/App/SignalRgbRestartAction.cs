928125174bef19fe2b7aa1bccca2119a1e37ae4e
Bill Z Bawb <bill.z.bawb.74@gmail.com>
2023-08-30 14:27:27 -0500

    feat: Implement SignalRGB Restart action

---
+++ SignalRgbDeckPlugin/Actions/App/SignalRgbRestartAction.cs
@@ -0,0 +1,15 @@
+﻿using BarRaider.SdTools;
+
+namespace SignalRgbDeckPlugin.Actions.App
+{
+    [PluginActionId("com.billzbawb.signalrgb.apprestart")]
+    public class SignalRgbRestartAction : SignalRgbKeypadBase
+    {
+        public override bool IsApplicationUrlValid => true;
+        public override string ApplicationUrl => "signalrgb://app/restart";
+
+        public SignalRgbRestartAction(ISDConnection connection, InitialPayload payload) : base(connection, payload)
+        {
+        }
+    }
+}
