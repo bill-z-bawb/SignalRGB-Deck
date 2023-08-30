60ba27f26595e7a0239297d2859708a2cf564869
Bill Z Bawb <bill.z.bawb.74@gmail.com>
2023-08-30 14:10:25 -0500

    feat: Implement SignalRGB plugins view URL

---
+++ SignalRgbDeckPlugin/Actions/Views/SignalRgbPluginsView.cs
@@ -0,0 +1,14 @@
+﻿using BarRaider.SdTools;
+
+namespace SignalRgbDeckPlugin.Actions.Views
+{
+    [PluginActionId("com.billzbawb.signalrgb.viewplugins")]
+    public class SignalRgbPluginsView : SignalRgbViewActionBase
+    {
+        public SignalRgbPluginsView(ISDConnection connection, InitialPayload payload) : base(connection, payload)
+        {
+        }
+
+        public override string ApplicationUrl => "signalrgb://view/userplugins";
+    }
+}
