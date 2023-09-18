2147c250b1572bb8f400b3e0b995054b6a00e23e
bill-z-bawb <128918988+bill-z-bawb@users.noreply.github.com>
2023-09-18 14:34:08 -0500

    feat: Add single action to restore last layout and effect at one time (#12)
    * feat: Add single action to restore last layout and effect at one time
    
    * spaces

---
+++ SignalRgbDeckPlugin/Actions/EffectAndLayoutCombo/SignalRgbPreviousEffectAndLayoutAction.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using BarRaider.SdTools;
+
+namespace SignalRgbDeckPlugin.Actions.EffectAndLayoutCombo
+{
+    [PluginActionId("com.billzbawb.signalrgb.previouseffectandlayout")]
+    public class SignalRgbPreviousEffectAndLayoutAction : SignalRgbKeypadBase
+    {
+        public override string[] ApplicationUrls
+        {
+            get
+            {
+                var effectUrl = new StringBuilder();
+                effectUrl.Append("signalrgb://effect/applyprevious");
+                effectUrl.Append($"?{SilentLaunchRequest}");
+
+                var layoutUrl = new StringBuilder();
+                layoutUrl.Append("signalrgb://layout/applyprevious");
+                layoutUrl.Append($"?{SilentLaunchRequest}");
+
+                return new[] { effectUrl.ToString(), layoutUrl.ToString() };
+            }
+        }
+
+        public SignalRgbPreviousEffectAndLayoutAction(ISDConnection connection, InitialPayload payload) : base(connection, payload)
+        {
+        }
+    }
+}
